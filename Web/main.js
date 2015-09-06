d9cce824df9ac1f3dc1012311f25fd89e3a678e3
Larry Xiao <xiaodi@sjtu.edu.cn>
2015-09-06 00:20:18 -0400

    web: drop down menu for choosing songs

--- Web/main.js
+++ Web/main.js
@@ -1,4 +1,10 @@
+// for prototype purpose
+list1 = ["Home", "Flickr", "Messenger", "Sports", "Finance", "Autos", "Beauty", "Movies"]
+list2 = ["Business", "Cricket", "Tech", "World", "News", "Support"]
+// start
+
 var playing = 0;
+
 
 function rotate () {
     if (playing) {
@@ -6,11 +12,26 @@ function rotate () {
     };
 }
 
+function addTrackMenu (track, arg) {
+    html = "<ul class=\"pure-menu-list\">";
+    for (var i = arg.length - 1; i >= 0; i--) {
+        html = html + "<li class=\"pure-menu-item\"><a href=\"#\" class=\"pure-menu-link\">"+arg[i]+"</a></li>";
+    };
+    html = html + "</ul>";
+    $(track).html(html);
+}
+
 document.addEventListener("DOMContentLoaded", function(event) { 
     console.log("ready");
     var cover_height = $(".splash-container").height() / 2;
     console.log("height:" + cover_height);
     $("#blank").height(cover_height/6);
+    $("#track1_button").click(function () {
+        $("#track1").toggle()
+    });
+    $("#track2_button").click(function () {
+        $("#track2").toggle()
+    });
     $("#play_button").click(function () {
         console.log("click")
         if (playing) {
@@ -29,4 +50,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
             $(".img-wrapper").css("-animation-play-state", "running");
         }
     }); 
+    updateTrackMenu();
 });
+
+// on query response
+function updateTrackMenu () {
+    addTrackMenu("#track1", list1);
+    addTrackMenu("#track2", list2);
+}
